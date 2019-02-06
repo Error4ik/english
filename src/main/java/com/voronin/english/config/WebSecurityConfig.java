c1fabd359aef010bf58320dce94e584b2aaed41a
Error4ik <fearerror@yandex.ru>
2019-02-06 18:38:24 +0300

    Added saving user tokens to the database #22

--- src/main/java/com/voronin/english/config/WebSecurityConfig.java
+++ src/main/java/com/voronin/english/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.voronin.english.config;
 
 import com.voronin.english.service.DetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,7 +11,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * Web security config.
@@ -42,13 +45,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     /**
+     * DataSource.
+     */
+    @Autowired
+    @Qualifier("dataSource")
+    private DataSource dataSource;
+
+    /**
      * Token store bean.
      *
      * @return in memory token store.
      */
     @Bean
     public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
+        return new JdbcTokenStore(dataSource);
     }
 
     /**
